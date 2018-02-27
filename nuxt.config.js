8dc8e42c336fe8bb61eb3fbbe53d08ffe8737732
Glitch (nuxt-with-element-ui) <none>
2018-02-27 10:44:16 +0000

    Exporting to ElementUI/nuxt-with-element-ui

---
+++ nuxt.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  /*
+  ** Global CSS
+  */
+  css: [
+    'element-ui/lib/theme-chalk/reset.css',
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+
+  /*
+  ** Add element-ui in our app, see plugins/element-ui.js file
+  */
+  plugins: [
+    '@/plugins/element-ui'
+  ]
+}
